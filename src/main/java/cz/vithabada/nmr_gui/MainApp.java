639eac825befb229ca3498710c069f5995f65ae4
Vit Habada <vithabada93@gmail.com>
2015-11-05 09:45:39 +0100

    spinAPI check

--- src/main/java/cz/vithabada/nmr_gui/MainApp.java
+++ src/main/java/cz/vithabada/nmr_gui/MainApp.java
@@ -4,8 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import libs.AlertHelper;
+import spinapi.SpinAPI;
 
 public class MainApp extends Application {
 
@@ -13,6 +16,15 @@ public class MainApp extends Application {
     public void start(Stage stage) throws Exception {
         stage.setMinWidth(900);
         stage.setMinHeight(500);
+
+        // check if SpinAPI is installed
+        try {
+            System.loadLibrary((System.getProperty("sun.arch.data.model").contains("64")) ? "spinapi64" : "spinapi");
+        } catch (Error error) {
+            AlertHelper.showAlert(Alert.AlertType.ERROR, "Missing SpinAPI", "It seems that SpinAPI is not installed on this computer. Please install SpinAPI first to use this software.");
+
+            return;
+        }
 
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
 
