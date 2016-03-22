c92a67572626d99bf362f7a9a88cca617ff36f37
Vit Habada <vithabada93@gmail.com>
2016-03-22 11:08:51 +0100

    refactor: radioprocessor class

--- src/main/java/model/RadioProcessor.java
+++ src/main/java/model/RadioProcessor.java
@@ -2,13 +2,31 @@ package model;
 
 
 import api.SpinAPI;
+import cz.vithabada.nmr_gui.pulse.Pulse;
+import org.apache.commons.math3.complex.Complex;
 
 public class RadioProcessor {
 
-    private boolean boardConnected;
+    private boolean boardConnected = false;
+
+    private boolean running = false;
+
+    private Pulse<Complex[]> pulse;
 
     public RadioProcessor() {
 
+    }
+
+    public Pulse<Complex[]> getPulse() {
+        return pulse;
+    }
+
+    public void setPulse(Pulse<Complex[]> pulse) throws Exception {
+        if(running) {
+            throw new Exception("Cannot set pulse while scan is running.");
+        }
+
+        this.pulse = pulse;
     }
 
     public boolean isBoardConnected() {
@@ -17,6 +35,10 @@ public class RadioProcessor {
 
     public void updateBoardStatus() {
         boardConnected = (SpinAPI.INSTANCE.pb_count_boards() > 0);
+    }
+
+    public boolean isRunning() {
+        return running;
     }
 
 }
